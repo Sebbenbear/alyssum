3d25f48d7754777df50c99bbf21089c693fdf7b5
Pauline Kelly <pauline.m.kelly1@gmail.com>
2016-08-06 20:47:27 +1000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  alyssum
+//
+//  Created by Pauline Kelly on 6/08/16.
+//  Copyright © 2016 Pauline Kelly. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
