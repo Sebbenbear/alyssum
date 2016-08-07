0f15cf9fd6f77c3d46ed7e87e73bb34ab143769a
Pauline Kelly <pauline.m.kelly1@gmail.com>
2016-08-07 14:03:40 +1000

    Set up core packages

--- main.swift
+++ main.swift
@@ -4,9 +4,15 @@
 //
 //  Created by Pauline Kelly on 6/08/16.
 //  Copyright © 2016 Pauline Kelly. All rights reserved.
+// 
+//  Reads in configuration, prints usage, etc.
+//
 //
 
 import Foundation
+import AlyssumCore
 
 print("Hello, World!")
 
+let p = Parser()
+p.parse()
